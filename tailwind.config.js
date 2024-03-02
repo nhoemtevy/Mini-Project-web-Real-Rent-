291621cfcd78ec31abddfecd5f2467aebefb5f52
tevynhoem <tevymomnh193@gmail.com>
2024-03-02 12:06:10 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./dist//*.{html,js}", "{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {
-  },
+      screens: {
+        'sm': '576px',
+        'md': '960px',
+        'lg': '1440px',
+      },
   },
   plugins: [],
 }
